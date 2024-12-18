d8d8245778ebabbc350ee59a00882c766f415748
Julius Lykkegaard <juliuslykkegaard@Juliuss-MacBook-Air.local>
2024-12-18 20:59:14 +0100

    initial commit

---
+++ src/main/java/com/example/infinityengineapplication/InfinityEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.infinityengineapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfinityEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infinityengineapplication/InfinityEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infinityengineapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
